1847ccd2071260cffe58c441c0cc5acf27c51bf5
Eric Hsieh <ehsiehweb@gmail.com>
2019-09-14 00:57:16 -0700

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ApolloClient from 'apollo-boost';
+import { gql } from 'apollo-boost';
 import * as serviceWorker from './serviceWorker';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',  
+});
+
+client
+  .query({
+    query: gql`
+    {
+      users
+      {
+        username
+      }
+    }
+    `
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
